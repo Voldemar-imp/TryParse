529e72a2c598d9d45cd5fb6e366d3c889f2c2a8b
Voldemar-imp <108013210+Voldemar-imp@users.noreply.github.com>
2022-07-18 13:46:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace staticVoid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = EnterToConvert();
+            Console.Clear();
+            Console.WriteLine("Вы ввели число: " + number);
+        }
+
+        static int EnterToConvert()
+        {             
+            int number = 0;
+            bool isContinue = true;
+
+            while (isContinue)
+            {
+                Console.Write("Введите целое число: ");
+                string userInput = Console.ReadLine();
+
+                bool success = int.TryParse(userInput, out number);
+                if (success== false)
+                {              
+                    Console.WriteLine($"Введенное значение: '{userInput}' не явлеяется целым числом, поробуйте еще раз.");
+                }         
+                
+                isContinue = !success;
+            }
+
+            return number;
+        }
+    }
+}
